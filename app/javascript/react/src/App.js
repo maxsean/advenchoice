bba1a7a0526e97eb25c2606946cd7e255521e9dd
maxsean <xsma1990@gmail.com>
2017-11-20 11:04:49 -0500

    practice redux basics

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import store from './stores/store';
+
+let unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+})
+console.log('Initial State of Store');
+console.log(store.getState());
+
+store.dispatch({ type: 'ADD_ONE' });
+store.dispatch({ type: 'RESET_TO_ZERO' });
+store.dispatch({ type: 'INCREASE_BY', numberToAdd: 2 });
+store.dispatch({ type: 'SUBSTRACT_BY', numberToSubtract: 2 });
 
 const App = props => {
   return(
