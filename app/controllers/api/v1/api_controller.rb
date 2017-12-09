cf668ccf24f0e35d146d87515e708b989eb782a3
maxsean <xsma1990@gmail.com>
2017-12-09 14:30:40 -0500

    modified routes and controllers

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,22 +1,19 @@
 class Api::V1::ApiController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+  helper_method :current_user
 
-    def authenticate_user_api!
-      if !user_signed_in?
-        render json: { error: "Not authorized" }, status: :unauthorized
-      end
-    end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
-    def render_object_errors(object)
-      render json: { error: object.errors }, status: :unprocessable_entity
-    end
-
-    private
-
-    def authorized_user
-      if doorkeeper_token
-        @authorized_user ||= User.find(doorkeeper_token.resource_owner_id)
-      end
+  def authenticate_user_api!
+    if !user_signed_in?
+      render json: { error: "Not authorized" }, status: :unauthorized
     end
   end
+
+  def render_object_errors(object)
+    render json: { error: object.errors }, status: :unprocessable_entity
+  end
+
 end
