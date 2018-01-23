6f27aa70f9ddf51d8be99a84cebdf9166993e381
maxsean <xsma1990@gmail.com>
2018-01-23 13:53:04 -0500

    restyled welcome page

--- app/javascript/react/src/components/Welcome.js
+++ app/javascript/react/src/components/Welcome.js
@@ -3,13 +3,19 @@ import React from 'react'
 const Welcome = (props) => {
   return(
     <div className='welcome'>
-      <div>
+      <div className='welcome-title'>
         <p>
-          Welcome to Advenchoice
+          Radiant Dawn
         </p>
-        <br/>
+      </div>
+      <div className='welcome-tile'>
         <p>
-          Please Sign In
+          By fire the world was undone. From the heavens a dark and gray shroud engulfed the North.
+          Perhaps you have seen it, what lies beyond the Ashen Veil. A murky, forgotten land where the cursed are coralled to mend their ailing souls.
+
+          One day you will stand before the veil, without really knowing why, like a moth drawn to a flame. You will lose everything, your past, your future, your very light. None will have meaning, and you won't even care.
+
+          Yet you would do anything to shed your curse. Then accept the fate of your ilk and face the trials that await you. 
         </p>
       </div>
     </div>
