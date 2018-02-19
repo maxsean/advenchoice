2c19ef7e825ae1465cb42f8dcea9b412b1fb478e
maxsean <xsma1990@gmail.com>
2018-02-19 13:36:34 -0500

    added admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202180914) do
+ActiveRecord::Schema.define(version: 20180219182503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20180202180914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "encounter", default: 1
+    t.boolean "admin"
   end
 
 end
